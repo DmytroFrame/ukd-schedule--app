4ff77c9f50e811b956551d33f72d0672b73a8fe8
vladyslav-lutchyn <vladyslav.lutchyn@vitechteam.com>
2023-03-03 01:40:21 -0500

    Redesigned

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { getSchedules } from "./api/getSchedules";
-
-import Schedule from "./components/Schedule/Schedule";
-import WeekBar from "./components/WeekBar/WeekBar";
+import {useQuery} from "react-query";
+import {Alert, Divider, Space, Spin, Tabs, Timeline, Typography} from 'antd';
+import axios from "axios";
+const { Title, Paragraph } = Typography;
 
 function App() {
-  const [schedules, setSchedules] = useState([]);
-  const [currentSchedule, setCurrentSchedule] = useState({});
+  const { isLoading, error, data } = useQuery("schedules", () =>
+      axios
+          .get("https://api.dmytroframe.site/ukd/schedules?group=ІПЗс-19")
+          .then((response) => response.data)
+  );
 
-  useEffect(() => {
-    getSchedules().then((res) => {
-      setSchedules(res);
-      setCurrentSchedule(res[0]);
-    });
-  }, []);
-
-  return (
+    return (
     <div className="App">
-      <WeekBar
-        schedules={schedules}
-        currentSchedule={currentSchedule}
-        setCurrentSchedule={setCurrentSchedule}
-      />
-      <Schedule currentSchedule={currentSchedule} />
+      <Typography>
+        <Title>Pidval project presents:</Title>
+          <Paragraph>
+              The most useless scheduler in your life.
+              Better go to stroika
+          </Paragraph>
+      </Typography>
+
+        <Divider />
+
+        { error &&
+            <Space
+                direction="vertical"
+                style={{
+                    width: '100%',
+                }}
+            ><Alert
+            message="Error"
+            description="Не вдається получити дані з серверу"
+            type="error"
+            showIcon
+        />
+            </Space>
+        }
+
+        { isLoading && (
+            <Space size="middle" style={{ marginTop: '50px'}}>
+                <Spin size="large" />
+            </Space>
+        )
+        }
+
+        {
+            !isLoading && (
+                <Tabs
+                    tabPosition={'left'}
+                    items={data?.map(_data => ({
+                        label: _data.weekName,
+                        key: Math.random(),
+                        children: <Timeline
+                            reverse
+                            items={_data.lessons.map(lesson => ({children: `${lesson.date} - ${lesson.title}`}))}
+                        />,
+                    }))}
+                />
+            )
+        }
+
+
     </div>
   );
 }
--- src/api/getSchedules.js
+++
@@ -1,6 +0,0 @@
-export async function getSchedules() {
-  const req = await fetch(
-    "https://api.dmytroframe.site/ukd/schedules?group=ІПЗс-19",
-  );
-  return  req.json();
-}
