5744cce7465ed3937d54495995f0b0337ad6e536
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2023-03-03 07:01:47 +0200

    Add new url to api

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import { getSchedules } from "./api/getSchedules";
 
 import Schedule from "./components/Schedule/Schedule";
 import WeekBar from "./components/WeekBar/WeekBar";
-import { mockSchedules } from "./api/mockSchedules";
 
 function App() {
-  const [schedules, setSchedules] = useState(mockSchedules);
-  const [currentSchedule, setCurrentSchedule] = useState(mockSchedules[0])
-
+  const [schedules, setSchedules] = useState([]);
+  const [currentSchedule, setCurrentSchedule] = useState({});
 
   useEffect(() => {
-    getSchedules().then((res) => {setSchedules(res); setCurrentSchedule(res[0])});
+    getSchedules().then((res) => {
+      setSchedules(res);
+      setCurrentSchedule(res[0]);
+    });
   }, []);
 
   return (
     <div className="App">
-      <WeekBar schedules={schedules} currentSchedule={currentSchedule} setCurrentSchedule= {setCurrentSchedule}/>
-      <Schedule currentSchedule={currentSchedule}/>
+      <WeekBar
+        schedules={schedules}
+        currentSchedule={currentSchedule}
+        setCurrentSchedule={setCurrentSchedule}
+      />
+      <Schedule currentSchedule={currentSchedule} />
     </div>
   );
 }
--- src/api/getSchedules.js
+++ src/api/getSchedules.js
@@ -1,14 +1,6 @@
-import { mockSchedules } from "./mockSchedules";
-
 export async function getSchedules() {
-  try {
-    const req = await fetch(
-      "https://tophot2022.online:7000/api/schedules?group=ІПЗс-19",
-    );
-    return await req.json();
-  } catch (error) {
-    return mockSchedules
-  }
-  // console.log(await req.json())
-  //
+  const req = await fetch(
+    "https://rozklad.dmytroframe.site/api/schedules?group=ІПЗс-19",
+  );
+  return  req.json();
 }
