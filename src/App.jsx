b3b15247e29f7d19081da3eaa8cf4ae8201df712
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2023-09-04 15:59:06 +0300

    Added dark theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import {useQuery} from "react-query";
-import {Alert, Divider, Space, Spin, Tabs, Timeline, Typography} from 'antd';
+import { useQuery } from "react-query";
+import { Divider, Typography, ConfigProvider, theme } from "antd";
 import axios from "axios";
 import ErrorAlert from "./components/Ui/ErrorAlert.jsx";
 import LoadingSpinner from "./components/Ui/LoadingSpinner.jsx";
@@ -9,27 +9,38 @@ import LessonsTab from "./components/LessonsTabs.jsx";
 const { Title, Paragraph } = Typography;
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => setIsDarkMode(event.matches));
+
   const { isLoading, error, data } = useQuery("schedules", () =>
-      axios
-          .get("https://api.dmytroframe.site/ukd/schedules?group=МІПЗс-23")
-          .then((response) => response.data)
+    axios
+      .get("https://api.dmytroframe.site/ukd/schedules?group=МІПЗс-23")
+      .then((response) => response.data)
   );
 
-    return (
-    <div className="App">
-      <Typography>
-        <Title>Pidval project presents:</Title>
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }}
+    >
+      <div className="App">
+        <Typography>
+          <Title>Pidval project presents:</Title>
           <Paragraph>
-              The most useless scheduler in your life.
-              Better go to stroika
+            The most useless scheduler in your life. Better go to stroika
           </Paragraph>
-      </Typography>
-      <Divider />
+        </Typography>
+        <Divider />
 
-      { error && <ErrorAlert /> }
-      { isLoading && <LoadingSpinner /> }
-      { data && <LessonsTab lessonsData={data} />}
-    </div>
+        {error && <ErrorAlert />}
+        {isLoading && <LoadingSpinner />}
+        {data && <LessonsTab lessonsData={data} />}
+      </div>
+    </ConfigProvider>
   );
 }
 
