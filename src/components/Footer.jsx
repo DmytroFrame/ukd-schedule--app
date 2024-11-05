f5e810dc96b92f39219d782d710ca56ca8d7257d
Dmytro Frame <dima.kostishen@gmail.com>
2024-11-05 09:59:52 +0200

    Added share button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,92 @@
 import React from "react";
-import { Typography } from "antd";
+import { Button, Modal, Typography } from "antd";
+import {
+  GlobalOutlined,
+  ShareAltOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 const { Link, Text } = Typography;
 
 export default function Footer() {
+  async function onShare() {
+    if (!navigator.share)
+      return alert("Shitt... this share button isn't working ╭∩╮(-_-)╭∩╮");
+
+    navigator
+      .share({
+        title: "Rozklad is here:",
+        url: "https://rozklad.dmytroframe.com",
+      })
+      .then((err) => console.error(err));
+  }
+
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <footer>
-      <Text> Authors </Text>
-
-      <Link href="https://github.com/DmytroFrame" target="_blank">
-        D. Kostyshyn
-      </Link>
-      <Text> & </Text>
-      <Link href="https://linkedin.com/in/vladyslav-lutchyn/" target="_blank">
-        V. Lutchyn
+      <Link onClick={onShare}>
+        <ShareAltOutlined /> Share
       </Link>
 
       <Text> - </Text>
+
+      <Link onClick={showModal}>
+        <UserOutlined /> Authors
+      </Link>
+
+      <Text> - </Text>
+
       <Link
         href="http://195.162.83.28/cgi-bin/timetable.cgi?n=700"
         target="_blank"
       >
-        Source
+        <GlobalOutlined /> Source
       </Link>
+
+      <Modal
+        title="Authors"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        width={320}
+        footer={[
+          <Button key="close" type="primary" onClick={handleCancel}>
+            Close
+          </Button>,
+        ]}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Text>
+            <Link href="https://github.com/DmytroFrame" target="_blank">
+              Dmytro Kostyshyn
+            </Link>
+            {" Backend"}
+          </Text>
+
+          <Text>
+            <Link
+              href="https://linkedin.com/in/vladyslav-lutchyn/"
+              target="_blank"
+            >
+              Vladyslav Lutchyn
+            </Link>
+            {" Frontend"}
+          </Text>
+        </div>
+      </Modal>
     </footer>
   );
 }
